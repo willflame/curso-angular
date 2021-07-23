bbf89e9c1d6e2de3252cb1a062a0d78ff1f8bbd9
Willian Anselmo da Silva <willian0506@hotmail.com>
2021-07-23 13:59:50 -0300

    Added validation to form

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -8,17 +8,38 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class CadastroFilmesComponent implements OnInit {
 
-  options: FormGroup;
+  cadastro: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
+  get f() {
+    return this.cadastro.controls;
+  }
+
   ngOnInit() {
 
-    this.options = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
+    this.cadastro = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      imageUrl: ['', [Validators.minLength(10)]],
+      releaseDate: ['', [Validators.required]],
+      description: [''],
+      imdbNote: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      imdbLink: ['', [Validators.minLength(10)]],
+      gender: ['', [Validators.required]]
     });
 
+  }
+
+  salve(): void {
+    if (this.cadastro.invalid) {
+      return;
+    }
+
+    alert("message");
+  }
+
+  resetForm() {
+    this.cadastro.reset();
   }
 
 }
