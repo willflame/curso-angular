89b20ef2e1b0c5d8b9f8096f50c426797d4c21d2
Willian Anselmo da Silva <willian0506@hotmail.com>
2021-07-27 08:55:02 -0300

    Created interface filme

---
+++ src/app/shared/models/filme.ts
@@ -0,0 +1,10 @@
+export interface Filme {
+  id?: number;
+  title: string;
+  imageUrl?: string;
+  releaseDate: Date;
+  description?: string;
+  imdbNote: number;
+  imdbLink: string;
+  gender: string;
+}
