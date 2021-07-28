01bdb7e7d54801c4f9b4ebbedd1c325c1500c24d
Willian Anselmo da Silva <willian0506@hotmail.com>
2021-07-28 09:05:40 -0300

    Created Form filter list films

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
+
+interface IGenderProps {
+  name: string;
+  value: string;
+}
 
 @Component({
   selector: 'dio-listagem-filmes',
@@ -11,12 +17,28 @@ export class ListagemFilmesComponent implements OnInit {
 
   readonly qtdPage = 4;
   page = 0;
-  filmes: Filme[];
+  filmes: Filme[] = [];
+  filterList: FormGroup;
+  gender: IGenderProps[];
 
-  constructor(private filmeService: FilmesService) { }
+  constructor(
+    private filmeService: FilmesService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.filterList = this.createFilterList();
     this.listFilms();
+
+    this.gender = [
+      { name: "Ação", value: "Ação"  },
+      { name: "Aventura", value: "Aventura"  },
+      { name: "Comédia", value: "Comédia"  },
+      { name: "Drama", value: "Drama"  },
+      { name: "Ficção Científica", value: "Ficção Científica"  },
+      { name: "Romance", value: "Romance"  },
+      { name: "Terror", value: "Terror"  },
+    ];
   }
 
   onScroll(): void {
@@ -26,7 +48,14 @@ export class ListagemFilmesComponent implements OnInit {
   private listFilms(): void {
     this.page++;
     this.filmeService.list(this.page, this.qtdPage).subscribe((filmes: Filme[]) => {
-      this.filmes?.push(...filmes);
+      this.filmes.push(...filmes);
+    });
+  }
+
+  private createFilterList(): FormGroup {
+    return this.fb.group({
+      text: [''],
+      gender: ['']
     });
   }
 }
